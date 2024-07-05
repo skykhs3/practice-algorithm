a5ba86c26a985c9f9cc7614e60c9521eb3baeff9
skykhs3 <skykhs3@naver.com>
2024-07-06 02:12:04 +0900

    feat: korasaju

--- 2. Baekjoon/Strongly_Connected_Component.cpp
+++ 2. Baekjoon/Strongly_Connected_Component.cpp
@@ -1,5 +1,84 @@
 #include <bits/stdc++.h>
 using namespace std;
+void createReverseEdge(int n,vector<vector<int>> &rE,vector<vector<int>> &E){
+  for(int i=1;i<=n;i++)
+    for(auto to:E[i])
+      rE[to].push_back(i);
+}
+
+void dfs(int v,int n,vector<vector<int>> &E, vector<bool> &visited, vector<int> &stk){
+  visited[v]=true;
+  for(auto to:E[v])
+    if(!visited[to]) dfs(to, n, E, visited, stk);
+  stk.push_back(v);
+}
+
+void rdfs(int v,int n,vector<vector<int>> &rE, vector<bool> &visited, vector<int> &scc){
+  visited[v]=true;
+  scc.push_back(v);
+  for(auto to:rE[v])
+    if(!visited[to]) rdfs(to, n, rE, visited, scc);
+}
+
+bool comp (const vector<int> lef, const vector<int> right){
+  return lef[0]<right[0];
+}
+
+void kosaraju(int n,vector<vector<int>> E){
+  vector<bool> visited(n+1);
+  vector<int> stck;
+
+  for(int i=1;i<=n;i++)
+    if(!visited[i]) dfs(i,n,E,visited,stck);
+  
+  vector<vector<int>> rE(n+1);
+  createReverseEdge(n,rE,E);
+
+  vector<vector<int>> ans;
+  fill(visited.begin(),visited.end(),false);
+  for(int i=stck.size()-1;i>=0;i--){
+    vector<int> scc;
+    if(!visited[stck[i]]){
+      rdfs(stck[i],n,rE,visited,scc);
+      ans.push_back(scc);
+    }
+  }
+
+for(auto &set:ans){
+    sort(set.begin(),set.end());
+    for(auto v:set){
+    //  printf("%d",v);
+    }
+    //printf("\n");
+  }
+
+  sort(ans.begin(),ans.end(),comp);
+
+  printf("%d\n",ans.size());
+  for(auto &set:ans){
+    for(auto v:set){
+      printf("%d ",v);
+    }
+    printf("-1\n");
+  }
+}
+
+int main(){
+  int n,m;
+  scanf("%d%d",&n,&m);
+  vector<vector<int>> E(n+1);
+  for(int i=1;i<=m;i++){
+    int x,y;
+    scanf("%d%d",&x,&y);
+    E[x].push_back(y);
+  }
+  kosaraju(n,E);
+}
+
+//Tarjan's algorithm
+/*
+#include <bits/stdc++.h>
+using namespace std;
 int dfs(int v, int n, vector<vector<int>>& E, vector<bool> &finished, vector<int> &d, int &cnt, stack<int> &st, vector<vector<int>> &ans){
   int p;
   p=d[v]= ++cnt;
@@ -73,3 +152,4 @@ int main(){
   }
   tarjan(n,V);
 }
+*/
