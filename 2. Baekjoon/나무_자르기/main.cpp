32f9b7f0b8ac831dc5fec935c818997ad50cf87b
skykhs3 <skykhs3@naver.com>
2024-08-27 01:28:08 +0900

    solved 나무자르기

--- 2. Baekjoon/나무_자르기/main.cpp
+++ 2. Baekjoon/나무_자르기/main.cpp
@@ -1,12 +1,20 @@
 #include <bits/stdc++.h>
 typedef long long ll;
 using namespace std;
-ll f(ll b,ll a,ll dp){
-  return b*a+dp;
-}
-// 틀린 이유를 찾아야해.
 
-ll oo=100000LL;
+struct Line {
+	ll a, b;
+	Line(ll a1, ll b1) :a(a1), b(b1) {}
+};
+
+ll f(Line x, ll v){
+  return x.a*v+x.b;
+}
+
+double cross(Line lef, Line rig){
+  return (double)(rig.b-lef.b)/(lef.a-rig.a);
+}
+
 void solve(){
   freopen("input1.txt","r",stdin);
   ll n;
@@ -18,24 +26,20 @@ void solve(){
   for(ll i=1;i<=n;i++){
     scanf("%lld",&b[i]);
   }
-  vector<ll> dp(n+1,oo);
-  vector<ll> deq(n+1);
-  ll s=1,t=1;
-  deq[1]=1;
-  dp[1]=0;
+  vector<ll> dp(n+1);
+  vector<Line> deq;
+  ll s=0,t=0;
+  deq.push_back(Line(b[1],0));
 
   for(ll i=2;i<=n;i++){
+    while(s+1<=t && f(deq[s],a[i])>=f(deq[s+1],a[i])) s++;
+    dp[i]=f(deq[s],a[i]);
 
-    while(s<t && f(b[deq[s]],a[i],dp[deq[s]])>=f(b[deq[s+1]],a[i],dp[deq[s+1]])) s++;
-
-    dp[i]=f(b[deq[s]],a[i],dp[deq[s]]);
-   // printf("i:%lld, dp[i]:%lld, %lld %lld\n",i,dp[i],deq[s],deq[t]);
-  //  printf("s:%lld, t:%lld\n",(dp[i]-dp[deq[t-1]])*(b[deq[t]]-b[i]),(dp[i]-dp[deq[t]])*(b[deq[t-1]]-b[i]));
-
-    while(s+1<=t &&
-          (double)(dp[deq[t-2]]-dp[deq[t-1]])/(b[deq[t-1]]-b[deq[t-2]])
-          >=(double)(dp[i]-dp[deq[t]])/(b[deq[t]]-b[i])) t--;
-    deq[++t]=i;
+    while(s+1<=t && cross(deq[t-1],deq[t])>=cross(deq[t],Line(b[i],dp[i]))){
+      deq.pop_back(); t--;
+    }
+    deq.push_back(Line(b[i],dp[i]));
+    t++;
   }
   printf("%lld",dp[n]);
 }
