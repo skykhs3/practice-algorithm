44bd68b6346cf5295c6f3a55c8333a3b79302919
skykhs3 <skykhs3@naver.com>
2024-08-27 18:18:17 +0900

    solve: scpc 2022 B

--- 3. codeground/최솟값,_중앙값,_최댓값.cpp
+++ 3. codeground/최솟값,_중앙값,_최댓값.cpp
@@ -5,92 +5,53 @@ using namespace std;
 ll oo=100000000000LL;
 vector<ll> A(100000+1);
 vector<ll> V(10000+1);
-ll M,K,Q,N;
-ll Min,Med,Max;
-ll mm;
-
-void three(){
-  
-  for(ll i=1;i<=Q;i++){
-    ll mn=oo;
-    for(ll j=1;j<=3;j++){
-      if(A[i]==V[j]){
-        if(j==1){
-          mn=min(mn,Min);
-        }
-        else if(j==2){
-          mn=min(mn,Med);
-        }
-        else{
-          mn=min(mn,Max);
-        }
-      }
-    }
-    if(mn==oo) printf("-1 ");
-    else printf("%lld ",mn);
-  }
-}
 
 void solve(){
+  ll M,K,Q,N;
+  ll Min,Med,Max;
+  ll Best;
+
   scanf("%lld%lld%lld",&M,&K,&Q);
   scanf("%lld%lld%lld",&Min,&Med,&Max);
-  mm=min({Min,Med,Max});
+  Best=min({Min,Med,Max});
   N=2*M*K+1;
-
-  
 
   for(ll i=1;i<=N;i++) scanf("%lld",&A[i]);
   sort(A.begin()+1,A.begin()+1+N);
-  vector<ll> an;
+  vector<ll> ansList;
   for(ll i=1;i<=Q;i++){
     scanf("%lld",&V[i]);
   }
-  // if(M==2 && K==1){
-  //   three();
-  //   return;
-  // }
 
-  for(ll i=1;i<=Q;i++){
-  
-    ll index1=lower_bound(A.begin()+1,A.begin()+1+N,V[i])-A.begin();
-    ll index2=upper_bound(A.begin()+1,A.begin()+1+N,V[i])-A.begin();
+  vector<ll> bestByNumber(100001,oo);
+  for(ll i=1;i<=N;i++){
     ll ans=oo;
-    if(index1<index2){
-      if((index1-1)%K!=0){
-        index1+=K-(index1-1)%K;
-      }
-
-      for(ll j=index1;j<=min(index2-1,index1+K);j+=K){
-        printf("%lld %lld\n",V[i],j);
-        ll x=j-1;
-        ll y=N-j;
-        if(x%K==0 && y%K==0 && x/K%2==y/K%2 ){
-          ll a=x/K%2,b=x/K/2,c=y/K/2;
-          ll temp = Med*a;
-          if(b==0){
-
-          }
-          else{
-            temp+=mm*(b-1)+Max;
-          }
-          if(c==0) {
-
-          } else{
-            temp+=mm*(c-1)+Min;
-          }
-          ans=min(ans,temp);
-        }
-      }
+    if(i==1) {
+      ans=min(ans,Min+Best*(M-1));
     }
-    an.push_back(ans==oo?-1:ans);
+    if(i==N){
+      ans=min(ans,Max+Best*(M-1));
+    }
+    if(1+K<=i && i<=N-K){
+      ans=min(ans,Med+Best*(M-1));
+    }
+    if(M>=2 && 2<=i && i<=N-2*K){
+      ans=min(ans,Min+min(Med,Max)+Best*(M-2));
+    }
+    if(M>=2 && 1+2*K<=i && i<=N-1){
+      ans=min(ans,Max+min(Med,Min)+Best*(M-2));
+    }
+    bestByNumber[A[i]]=min(bestByNumber[A[i]],ans);
   }
 
-  for(ll i=0;i<an.size();i++)
-    printf("%lld ",an[i]);
+  for(ll i=1;i<=Q;i++){
+    if(bestByNumber[V[i]]==oo) printf("-1 ");
+    else printf("%lld ",bestByNumber[V[i]]);
+  }
 }
 int main(){
   ll T;
- // setbuf(stdout, NULL); 오히려 이것 때문에 자주 출력해서 시간 초과가 날수 있다.
+  freopen("input.txt","r",stdin);
   scanf("%lld",&T);
   for(ll i=1;i<=T;i++){
     printf("Case #%lld\n",i);
