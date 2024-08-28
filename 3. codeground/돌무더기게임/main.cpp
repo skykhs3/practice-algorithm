97ac0ac8a27124feccb24451592ece43497233e8
skykhs3 <skykhs3@naver.com>
2024-08-28 11:13:39 +0900

    failed: 돌무더기게임

--- 3. codeground/돌무더기게임/main.cpp
+++ 3. codeground/돌무더기게임/main.cpp
@@ -6,24 +6,35 @@ ll solve(){
   scanf("%lld",&n);
   vector<ll> a(n+1);
   vector<vector<ll>> dp(n+2,vector<ll>(n+2));
+  vector<vector<ll>> memoK(n+2,vector<ll>(n+2));
   for(ll i=1;i<=n;i++) scanf("%lld",&a[i]);
-  for(ll i=1;i<=n;i++) dp[i][i]=a[i];
+  for(ll i=1;i<=n;i++) dp[i][i]=a[i],memoK[i][i]=i;
 
   for(ll gap=2;gap<=n;gap++){
     for(ll s=1;s+gap-1<=n;s++){
       ll e=s+gap-1;
-      for(ll k=s;k<=e;k++){
-        if(dp[s][e]<a[k]+min(dp[s][k-1],dp[k+1][e])) dp[s][e]=a[k]+min(dp[s][k-1],dp[k+1][e]);
+      for(ll k=memoK[s][e-1];k<=memoK[s+1][e];k++){
+        if(dp[s][e]<a[k]+min(dp[s][k-1],dp[k+1][e])){
+            dp[s][e]=a[k]+min(dp[s][k-1],dp[k+1][e]);
+            memoK[s][e]=k;
+        }
       }
     }
   }
-
-  for(ll i=1;i<=n;i++){
-    for(ll j=1;j<=n;j++){
-      printf("%3lld ",dp[i][j]);
-    }
-    printf("\n");
-  }
+//
+//  for(ll i=1;i<=n;i++){
+//    for(ll j=1;j<=n;j++){
+//      printf("%3lld ",dp[i][j]);
+//    }
+//    printf("\n");
+//  }
+//  printf("\n");
+//  for(ll i=1;i<=n;i++){
+//    for(ll j=1;j<=n;j++){
+//      printf("%3lld ",memoK[i][j]);
+//    }
+//    printf("\n");
+//  }
 
   return dp[1][n];
 }
