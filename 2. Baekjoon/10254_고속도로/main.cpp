d77f752bd7f7b2010688338aff79d14d4e12f081
skykhs3 <skykhs3@naver.com>
2024-08-30 22:32:32 +0900

    timeout: 고속도로

---
+++ 2. Baekjoon/10254_고속도로/main.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+struct vector2{
+  int x,y;
+  vector2(int _x=0,int _y=0): x(_x),y(_y){};
+  bool operator <(const vector2 &rig)const{
+     return y==rig.y ? x<rig.x : y<rig.y;
+  }
+  vector2 operator - (const vector2 &rhs) const{
+    return vector2(x-rhs.x, y-rhs.y);
+  }
+  vector2 operator - () const{
+    return vector2(-x, -y);
+  }
+  ll dot(const vector2 &rhs){
+    return x*rhs.x+y*rhs.y;
+  }
+  ll normSquare(){
+    return x*x+y*y;
+  }
+};
+
+vector2 standard(0,0);
+
+vector<vector2> points;
+vector<vector2> st;
+vector<vector2> toNext;
+
+ll crossProduct(const vector2 &v0,const vector2 &v1,const vector2 &v2){
+  return (v1.x-v0.x)*(v2.y-v0.y)-(v2.x-v0.x)*(v1.y-v0.y);
+}
+
+ll dist2(const vector2 &v0,const vector2 &v1){
+  return (v1.x-v0.x)*(v1.x-v0.x)+(v1.y-v0.y)*(v1.y-v0.y);
+}
+
+bool cmp2(const vector2 &lef,const vector2 &rig){
+  ll d=crossProduct(standard,lef,rig);
+  if(d>0) return true;
+  if(d<0) return false;
+  return dist2(standard,lef)<dist2(standard,rig);
+}
+
+bool clockWise(const vector2 &v0,const vector2 &v1,const vector2 &v2){
+  ll d=crossProduct(v0,v1,v2);
+  if(d<0) return true;
+  if(d>0) return false;
+  return dist2(v0,v2)>dist2(v0,v1);
+}
+
+void diameter(vector<vector2> &p){
+  int n=p.size();
+  int lef=min_element(p.begin(),p.end())-p.begin();
+  int rig=max_element(p.begin(),p.end())-p.begin();
+
+  vector2 calipersA(0,1);
+  ll ret=(p[rig]-p[lef]).normSquare();
+  vector<int> ans={lef,rig};
+
+  toNext.resize(n);
+  for(int i=0;i<n;i++)
+    toNext[i]=(p[(i+1)%n]-p[i]);
+
+  int a=lef, b=rig;
+  while(a!=rig || b!=lef){
+    ll dotA=calipersA.dot(toNext[a]);
+    ll dotB=-calipersA.dot(toNext[b]);
+    if(dotA*dotA*toNext[b].normSquare()>dotB*dotB*toNext[a].normSquare()){
+      calipersA=toNext[a];
+      a=(a+1)%n;
+    } else{
+      calipersA=-toNext[b];
+      b=(b+1)%n;
+    }
+    if(ret<(p[a]-p[b]).normSquare()){
+      ret=(p[a]-p[b]).normSquare();
+      ans={a,b};
+    }
+  }
+  printf("%d %d %d %d\n",p[ans[0]].x,p[ans[0]].y,p[ans[1]].x,p[ans[1]].y);
+}
+
+
+void solve(){
+  int n;
+  scanf("%d",&n);
+  points.resize(n);
+  for(int i=0;i<n;i++) scanf("%d%d",&points[i].x,&points[i].y);
+
+  sort(points.begin(),points.end());
+
+  standard=points[0];
+  sort(points.begin()+1,points.end(),cmp2);
+
+  st.clear();
+  st.push_back(points[0]);
+  for(int i=1;i<n;i++){
+    while(st.size()>=2 && clockWise(st[st.size()-2],st[st.size()-1],points[i])) st.pop_back();
+    st.push_back(points[i]);
+  }
+
+  diameter(st);
+}
+
+int main(){
+  freopen("input.txt","r",stdin);
+  int test;
+  scanf("%d",&test);
+  //for(ll i=1;i<=test;i++) solve();
+  return 0;
+}
