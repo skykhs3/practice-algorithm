dcd60aaf778f63b5cb7419278afb5301fb068391
skykhs3 <skykhs3@naver.com>
2024-10-07 17:59:21 +0900

    time out

--- 8. codetree/코드트리_채점기.cpp
+++ 8. codetree/코드트리_채점기.cpp
@@ -1,11 +1,13 @@
-// unordered_map 사용법 알기.
+// string은 인덱스에 넣지 말자.
+// string은 서로 비교를 하지 말자
+// string은 복사를 하지 말자.
 #include <bits/stdc++.h>
 using namespace std;
 typedef int ll;
 struct Problem{
   ll priority;
   ll time;
-  string url;
+  ll urlID;
   bool operator < (const Problem &r) const{
     return priority>r.priority || (priority==r.priority &&  time>r.time);
   }
@@ -13,15 +15,19 @@ struct Problem{
 ll n;
 ll oo=987654321;
 
-unordered_set<string> url;
-unordered_map<string,priority_queue<Problem>> Q;
+unordered_set<ll> url;
+vector<priority_queue<Problem>> Q(50001);
 
-unordered_map<string,ll> startTime;
-unordered_map<string,ll> endTime;
+vector<ll> startTime(50001);
+vector<ll> endTime(50001);
 
 priority_queue<ll,vector<ll>,greater<ll>> solver;
-vector<string> solverState(50001);
+vector<ll> solverState(50001);
 
+map<string,ll> wholeUrl;
+map<string,ll> wholeDomain;
+ll cnt=0;
+ll cnt2=0;
 
 string makeP(string u0){
   int i=u0.find('/');
@@ -31,9 +37,12 @@ void ord100(){
   string u0;
   cin>>n>>u0;
   string domain=makeP(u0);
-  if(url.find(u0)==url.end()){
-    url.insert(u0);
-    Q[domain].push({1,0,u0});
+  if(wholeUrl.find(u0)==wholeUrl.end()) wholeUrl[u0]=++cnt;
+  if(wholeDomain.find(domain)==wholeDomain.end()) wholeDomain[domain]=++cnt2;
+
+  if(url.find(wholeUrl[u0])==url.end()){
+    url.insert(wholeUrl[u0]);
+    Q[wholeDomain[domain]].push({1,0,wholeUrl[u0]});
   }
   for(ll i=1;i<=n;i++) solver.push(i);
 }
@@ -42,53 +51,58 @@ void ord200(){
   string u;
   cin>>t>>p>>u;
   string domain=makeP(u);
+  if(wholeDomain.find(domain)==wholeDomain.end()) wholeDomain[domain]=++cnt2;
+  if(wholeUrl.find(u)==wholeUrl.end()) wholeUrl[u]=++cnt;
 
-  if(url.find(u)==url.end()){
-    url.insert(u);
-    Q[domain].push({p,t,u});
+  if(url.find(wholeUrl[u])==url.end()){
+    url.insert(wholeUrl[u]);
+    Q[wholeDomain[domain]].push({p,t,wholeUrl[u]});
   }
 }
 void ord300(){
   ll t;
   cin>>t;
-  string v="-";
+  ll v=0;
   Problem mx;
   if(solver.empty()) return;
 
-  for(auto &it:Q){
-    string domain=it.first;
-    ll start=startTime[domain];
-    ll end=endTime[domain];
+  for(ll i=1; i<=cnt2; i++){
+   
+    ll start=startTime[i];
+    ll end=endTime[i];
     ll gap=end-start;
     if(end==oo || !(end==0 || (t>=start+3*gap))) continue;
-    auto &que=it.second;
+
+    auto &que=Q[i];
     if(!que.empty()){
+  
       Problem top=que.top();
-      if(v=="-" || mx<top){
+      if(v==0 || mx<top){
         mx=top;
-        v=domain;
+        v=i;
       }
     }
   }
 
-  if(v!="-"){
+
+  if(v!=0){
     ll index=solver.top();solver.pop();
     startTime[v]=t;
     endTime[v]=oo;
     solverState[index]=v;
-
     auto &que=Q[v];
-    url.erase(que.top().url);
+    url.erase(que.top().urlID);
     que.pop();
   }
 }
 void ord400(){
   ll t, jid;
   cin>> t>>jid;
-  if(solverState[jid]=="") return;
-  string domain=solverState[jid];
-  endTime[domain]=t;
-  solverState[jid]="";
+  if(solverState[jid]==0) return;
+  ll domainID=solverState[jid];
+
+  endTime[domainID]=t;
+  solverState[jid]=0;
   solver.push(jid);
 }
 void ord500(){
